b0f29f63ac17ebb8e9740e03cb43f31e6340b1fd
hemal507 <hchowdam@gmail.com>
2026-02-04 07:54:13 +0530

    Time: 48 ms (16.73%) | Memory: 19.3 MB (40.55%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
@@ -7,8 +7,8 @@
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        left = 0
-        right = n - 1
+        left = 1
+        right = n
         while left <= right :
             mid = (left + right) // 2
             if guess(mid) == 0 :
@@ -17,4 +17,3 @@ class Solution:
                 right = mid - 1
             else :
                 left = mid + 1
-        return left
