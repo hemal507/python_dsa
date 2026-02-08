f401b25345e7d59542497ae537427a7bb11e270f
hemal507 <hchowdam@gmail.com>
2026-02-08 22:24:52 +0530

    Time: 1074 ms (90.76%) | Memory: 33.3 MB (53.92%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        if hour <= len(dist) - 1 :
+            return -1
+        def can_reach(x) :
+            h = 0
+            for i in dist[:-1] :
+                h += math.ceil(i / x)
+            h += (dist[-1] / x)
+            print(h)
+            return h <= hour
+
+        left = 1
+#sum(dist) failed ex 100000  and left over time is 0.01 , to reach speed should be 100000 * (100)
+        right = 10000000    
+        ans = -1
+        while left <= right :
+            mid = (left + right) // 2
+            if can_reach(mid) :
+                ans = mid
+                right = mid - 1
+            else :
+                left = mid + 1
+        return ans
