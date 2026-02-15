6bdab36ae5b4141e07a829a2ca4107a3d779b40e
hemal507 <hchowdam@gmail.com>
2026-02-15 21:28:16 +0530

    Time: 1 ms (86.94%) | Memory: 20.6 MB (52.15%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(numbers)) :
+            diff = target - numbers[i] 
+            if diff in d :
+                return [ d[diff]+1, i+1 ]
+
+            d[numbers[i]] = i
