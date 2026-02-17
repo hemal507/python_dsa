a180cd14b9c5ba2f3456dc8ea70ee0ab05aa7db8
hemal507 <hchowdam@gmail.com>
2026-02-17 12:58:35 +0530

    Time: 15 ms (2.54%) | Memory: 19.6 MB (18.45%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,16 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        right = len(nums) - 1
-        mid = 0
-        while mid <= right :
-            if nums[mid] == 0 :
-                nums[mid], nums[left] = nums[left], nums[mid]
-                left += 1
-                mid += 1
-            elif nums[mid] == 1 :
-                mid += 1
-            elif nums[mid] == 2 :
-                nums[mid], nums[right] = nums[right], nums[mid]
-                right -= 1
+        pointer_0 = 0
+        pointer_1 = 0
+        pointer_2 = len(nums) - 1
+        
+        while pointer_1 <= pointer_2 :
+            print('bfr', nums, pointer_0, pointer_1, pointer_2)
+            if nums[pointer_1] == 0 :
+                nums[pointer_1], nums[pointer_0] = nums[pointer_0], nums[pointer_1]
+                pointer_0 += 1
+                pointer_1 += 1
+                print('at0', nums, pointer_0, pointer_1, pointer_2)
+            elif nums[pointer_1] == 1 :
+                pointer_1 += 1
+                print('at1', nums, pointer_0, pointer_1, pointer_2)
+            elif nums[pointer_1] == 2 :
+                nums[pointer_1], nums[pointer_2] = nums[pointer_2], nums[pointer_1]
+                pointer_2 -= 1
+                print('at2', nums, pointer_0, pointer_1, pointer_2)
+            # print('aft', nums, pointer_0, pointer_1, pointer_2)
+            print("")
