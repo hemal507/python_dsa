fedaf9f19488f9efa8c9205e18af7307655df08c
hemal507 <hchowdam@gmail.com>
2026-02-28 22:21:13 +0530

    Create 151-Reverse-Words-in-a-String.py

---
+++ 151-Reverse-Words-in-a-String.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        i = len(s) - 1
+        r = []
+
+        while i >= 0 :
+            while i >= 0 and s[i] == ' ' :
+                i -= 1
+
+            if i < 0 :
+                break
+
+            j = i
+            while i >= 0 and s[i] != ' ':
+                i -= 1
+
+            r.append(s[i+1:j+1])
+
+        return ' '.join(r)
